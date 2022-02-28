0c5f1a0a424a7a3af0177d9798700b57a8079686
GeorgeHutchinson1627 <82395601+GeorgeHutchinson1627@users.noreply.github.com>
2022-02-28 10:22:11 -0800

    Update possible_library.js

--- assignment4/possible_library.js
+++ assignment4/possible_library.js
@@ -1,1 +1,19 @@
+//when we have actual server this will work better.
+//also need to know more about how to use databases 
 
+//this can get a username and  password from a user database
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username, password: password }, function (err, user) {
+      done(err, user);
+    });
+  }
+));
+
+//this redicrects user back to the home page.
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Authentication successful. Redirect home.
+    res.redirect('/');
+  });
