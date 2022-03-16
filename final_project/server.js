62c31a72293bbe3f27c3969532d92400bf9a812b
GeorgeHutchinson1627 <82395601+GeorgeHutchinson1627@users.noreply.github.com>
2022-03-15 20:37:00 -0700

    Create server.js

---
+++ final_project/server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+//Redirects to chat page
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('New user joined the chat');
+
+  socket.on('codeboard-message', (msg) => {
+    console.log('message: ' + msg);
+  	socket.broadcast.emit('codeboard-message-broadcasted', msg);
+  });
+  
+});
+
+var server_port = process.env.PORT || 3000;
+http.listen(server_port, () => {
+  console.log("server is up");
+});
